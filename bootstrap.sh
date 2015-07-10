5585f534c6d06d9ce7d144aa2531685210136469
Thiago Vinhas <thiago@vinhas.org>
2015-07-09 22:44:38 -0400

    bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+# Checks to see if being run by root
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root. Type \'sudo su -\' and try again." 1>&2
+   exit 1
+fi
+
+# Update packages and install Chef and its dependencies
+yum update -y
+yum install -y gcc automake autoconf libtool make ruby ruby-devel ruby-rdoc rubygems git
+
+# Install cookbook 
+if [ ! -d /root/chef_code ]             
+  then
+    mkdir /root/chef_code
+fi
+
+rm -fR {/tmp/git,/root/chef_code/cookbooks}
+git clone https://github.com/tvinhas/samsung /tmp/git ; mv -fu /tmp/git/cookbooks /root/chef_code/cookbooks ; rm -fR /tmp/git
+
+# Install chef-solo
+gem install chef ruby-shadow --no-ri --no-rdoc
+
+if [ ! -d /etc/chef ]      
+  then
+    mkdir /etc/chef
+fi
+
+ echo '
+  file_cache_path "/root/chef_code"
+  cookbook_path "/root/chef_code/cookbooks"
+  json_attribs "/etc/chef/node.json"
+' > /etc/chef/solo.rb
+
+ echo '
+{
+  "run_list": [ "recipe[samsung]" ]
+}
+' > /etc/chef/node.json
+
+chef-solo
